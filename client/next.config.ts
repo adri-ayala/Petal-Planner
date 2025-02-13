32b9a249f1fda45b6adc795bd99dfbcf1e630dc0
adri-ayala <Adrienne.ayala.c@gmail.com>
2025-02-13 00:30:28 -0500

    s3 fix config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "petal-s3-img.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname:"/**",
+      },
+    ],
+  },
 };
+
 
 export default nextConfig;
--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -5,16 +5,8 @@ const config = {
   plugins: {
     tailwindcss: {},
   },
-  images:{
-    remotePatterns:[
-      {
-        protocol: "https",
-        hostname: "petal-s3-img.s3.us-east-1.amazonaws.com",
-        port: "",
-        pathname:"/**",
-      }
-    ]
+
   }
-};
+
 
 export default config;
